deffb0616fe6c66f2b17340d7b6ac2d0742f069b
Mecha-Vinay <vinay.flipgrid@gmail.com>
2025-09-09 07:42:38 +0530

    Add files via upload

---
+++ vinay.py/things.py/arbituary_arg.py
@@ -0,0 +1,31 @@
+# *args = allows you to pass muliple non-key arguments (is a tuple)
+# **kwargs = allows you to pass multiple keyword srguments 
+#           * unpacking operator(ex-*args,*num,*anything)
+
+
+#def add(*args):
+#    total = 0
+#    for arg in args:
+#        total += arg
+#    return total
+#print(add(1,2,3,4,4,4,4))
+
+
+
+#def dislplay_name(*args):
+#    for arg in args:
+#        print(arg, end=" ")
+
+
+#dislplay_name("Mr.","vinay","the destroyer of worlds")
+
+
+
+#def print_address(**kwargs):
+#    for key, value in kwargs.items():
+#        print(f"{key} : {value}")
+
+#print_address(street="123 main st.",
+#               city="ohio",
+#               state="USA",
+#               zip=12345)
